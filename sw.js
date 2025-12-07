6a7e2e848f836930fed1cb11658397bca4a6f68d
Juan Antonio Zen <nomedesenlacabeza@gmail.com>
2025-12-07 10:27:36 +0100

    fix: ruta pwa

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
 const CACHE_NAME = "decisiones-v3";
 const ASSETS = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/app.js",
-  "/manifest.webmanifest",
-  "/icons/icon.svg",
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icons/icon.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
