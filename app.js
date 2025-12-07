2322d14de3c901b234ebfb2c772bb407d5de2d3d
Juan Antonio Zen <nomedesenlacabeza@gmail.com>
2025-12-07 10:32:20 +0100

    fix: otra ruta

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ setState({ label: "Listo", bodyClass: "" });
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("./sw.js")
       .catch((error) => console.error("SW registration failed", error));
   });
 }
